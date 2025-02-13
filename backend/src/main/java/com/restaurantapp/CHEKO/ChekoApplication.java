9eb7bca546115a86dbf554fa4670278aff5b530c
badr <badrbm540@gmail.com>
2025-02-13 22:15:56 +0300

    first commit

---
+++ backend/src/main/java/com/restaurantapp/CHEKO/ChekoApplication.java
@@ -0,0 +1,13 @@
+package com.restaurantapp.CHEKO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChekoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChekoApplication.class, args);
+	}
+
+}
